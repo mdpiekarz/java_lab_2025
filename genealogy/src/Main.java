d8eac0028fa9bfbed50ffa1507eaf23832e2dd95
mdpiekarz <monika.piekarz@mail.umcs.pl>
2025-05-14 16:13:37 +0200

    lab 9

--- genealogy/src/Main.java
+++ genealogy/src/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-            List<Person> osoby = new ArrayList<>();
+            List<Person> osoby;
 //            Person jan = new Person("Jan", "Kowalski", LocalDate.of(1990, 5, 20),null);
 //            Person anna = new Person("Anna", "Nowak", LocalDate.of(1985, 10, 15),null);
 //            Person piotr = new Person("Piotr", "Zielinski", LocalDate.of(2000, 3, 8),null);
@@ -49,22 +49,36 @@ public class Main {
 //            System.out.println(p);
 //        }
 
-        //-----------lab6-start-------------------------------------------------------------------------
-        System.out.println(Person.fromCsvLine("Marek Kowalski,15.05.1899,25.06.1957,,"));
-        try {
-            osoby=Person.fromCsv("family.csv");
-        } catch (IOException e) {
-            System.err.println("Błąd podczas odczytu linii: " + e.getMessage());;
-        }
-        System.out.println(osoby);
+        //lab6-start-------------------------------------------------------------------------
+        //System.out.println(Person.fromCsvLine("Marek Kowalski,15.05.1899,25.06.1957,,"));
 
-        System.out.println("_____________Zapis i odczyt listy obiektów Person - pliki binarne__________________________________________");
+        osoby=Person.fromCsv("family.csv");
 
-        Person.toBinaryFile(osoby, "family.bin");
-        List<Person> loadPeople = Person.fromBinaryFile("family.bin");
-        for(Person person: loadPeople)
-            System.out.println(person);
+        //System.out.println(osoby);
 
+//        Person.toBinaryFile(osoby, "family.bin");
+//        List<Person> loadPeople = Person.fromBinaryFile("family.bin");
+//        for(Person person: loadPeople)
+//            System.out.println(person);
+
+//_____________lab 7_____________________________________
+        PlantUMLRunner.setPath("plantuml-1.2025.2.jar");
+//        PlantUMLRunner.generate(osoby.get(0).toPlantUMLTree(), "out_uml", "family");
+//        PlantUMLRunner.generate(Person.toPlantUMLTree(osoby), "out_uml", "allFamily");
+//__________________lab 9 _____________________________________
+        System.out.println(Person.filterByName(osoby, "Nowak"));
+        System.out.println(Person.sortByBirth(osoby));
+        System.out.println("zmarli:" + Person.sortByLifespanDesc(osoby));
+        //osoby = Person.sortByLifespanDesc(osoby);
+        System.out.println("Najstarsza żyjąca: " +Person.findOldestPersonAlive(osoby));
+
+        PlantUMLRunner.generate(Person.toPlantUMLTreeV2(osoby,
+                    str -> str + " #6BBF59"
+            ), "out_uml", "familyColor");
+
+        PlantUMLRunner.generate(Person.toPlantUMLTreeV3(osoby,
+                    person -> person == Person.findOldestPersonAlive(osoby),
+                    str -> str + " #6BBF59"
+            ), "out_uml", "familyAlive");
     }
-
 }
--- genealogy/src/Person.java
+++ genealogy/src/Person.java
@@ -4,6 +4,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Person implements Comparable<Person>, Serializable{
@@ -151,7 +152,7 @@ public class Person implements Comparable<Person>, Serializable{
 
     @Override
     public String toString() {
-        return imie + " " + nazwisko + ", urodzony: " + dataUrodzin + " dzieci: " + children;
+        return imie + " " + nazwisko + ", urodzony: " + dataUrodzin + ", data śmierci: "+ dataSmierci;// +", dzieci: " + children;
     }
 
     @Override
@@ -166,7 +167,7 @@ public class Person implements Comparable<Person>, Serializable{
         return sortedChildren;
     }
 
-    /*------------------lab 7-start-----------------------------------------------------*/
+    /*------------------lab 7------------------------------------------------------*/
     public String toPlantUMLTree() {
         String result = "@startuml\n%s\n%s\n@enduml";
         Function<String, String> objectLine = str -> String.format("object \"%s\" as %s\n",str, str.replaceAll(" ", ""));
@@ -219,8 +220,110 @@ public class Person implements Comparable<Person>, Serializable{
         return String.format(result, objects, relations);
     }
 
+    public static List<Person> filterByName(List<Person> people, String nameSubstring) {
+        return people.stream()
+                .filter(person -> person.nazwisko.contains(nameSubstring))
+                .collect(Collectors.toList());
+    }
 
-    // rozwiązanie bez Consumer
+//    public static List<Person> sortByBirth(List<Person> people) {
+//        people.sort(Comparator.comparing(person -> person.dataUrodzin));
+//        return people;
+//    }
+
+    public static List<Person> sortByBirth(List<Person> people) {
+        return people.stream()
+                .sorted()
+                .collect(Collectors.toList());
+    }
+
+    public static List<Person> sortByLifespanDesc(List<Person> people) {
+        return people.stream()
+                .filter(person -> person.dataSmierci != null)
+                .sorted((person1, person2) -> {
+                    long lifespan1 = person1.dataSmierci.toEpochDay() - person1.dataUrodzin.toEpochDay();
+                    long lifespan2 = person2.dataSmierci.toEpochDay() - person2.dataUrodzin.toEpochDay();
+                    return Long.compare(lifespan2, lifespan1);
+                })
+                .collect(Collectors.toList());
+    }
+
+    public static Person findOldestPersonAlive(List<Person> people) {
+        return people.stream()
+                .filter(person -> person.dataSmierci == null)
+                .min(Comparator.comparing(Person::getDataUrodzin))
+                .orElse(null);
+    }
+
+    //Zad 8
+    public static String toPlantUMLTreeV2(List<Person> people, Function<String, String> postProcess) {
+        String result = "@startuml\n%s\n%s\n@enduml";
+        Function<String, String> objectLine = str -> String.format("object \"%s\" as %s", str, str.replaceAll(" ", ""));
+        Function<String, String> objectLineAndPostprocess = objectLine.andThen(postProcess);
+        Function<String[], String> relationLine = pair -> String.format("%s<--%s\n", pair[0], pair[1]);
+
+        StringBuilder objects = new StringBuilder();
+        StringBuilder relations = new StringBuilder();
+        Set<String> addedObjects = new HashSet<>();
+        Set<String> addedRelations = new HashSet<>();
+
+        Consumer<Person> addPerson = person -> {
+            String personKey = person.getImie() + " " + person.getNazwisko();
+            if (addedObjects.add(personKey)) {
+                objects.append(objectLineAndPostprocess.apply(personKey)+"\n");
+            }
+
+            for (Person child : person.getChildren()) {
+                String childKey = child.getImie() + " " + child.getNazwisko();
+                if (addedObjects.add(childKey)) {
+                    objects.append(objectLineAndPostprocess.apply(childKey)+"\n");
+                }
+
+                String[] relationPair = new String[] { person.imie+person.nazwisko, child.imie+child.nazwisko };
+                String relationStr = relationLine.apply(relationPair);
+                if (addedRelations.add(relationStr)) {
+                    relations.append(relationStr);
+                }
+            }
+        };
+
+        people.forEach(addPerson);
+
+        return String.format(result, objects, relations);
+    }
+
+
+    //Zad 8 i 9
+    public static String toPlantUMLTreeV3(List<Person> people, Predicate<Person> condition, Function<String, String> postProcess) {
+        String result = "@startuml\n%s\n%s\n@enduml";
+        Function<String, String> objectLine = str -> String.format("object \"%s\" as %s", str, str.replaceAll(" ", ""));
+        Function<String, String> objectLineAndPostprocess = objectLine.andThen(postProcess);
+
+        Map<Boolean, List<Person>> groupedPeople = people.stream().collect(Collectors.partitioningBy(condition));
+
+        Set<String> objects = groupedPeople.get(true).stream()
+                .map(person -> (person.imie + " "+ person.nazwisko))
+                .map(objectLineAndPostprocess)
+                .collect(Collectors.toSet());
+
+        objects.addAll(groupedPeople.get(false).stream()
+                .map(person -> (person.imie + " "+ person.nazwisko))
+                .map(objectLine)
+                .collect(Collectors.toSet())
+        );
+
+        Set<String> relations = people.stream()
+                .flatMap(person -> person.children.stream()
+                        .map(child -> ((child.imie + child.nazwisko) + "<--" + (person.imie + person.nazwisko))))
+                .collect(Collectors.toSet());
+
+        String objectString = String.join("\n", objects);
+        String relationString = String.join("\n", relations);
+
+        return String.format(result, objectString, relationString);
+    }
+
+    //Zad 3 bez Consumer
     //    public static String toPlantUMLTree(List<Person> people) {
 //        String result = "@startuml\n%s\n%s\n@enduml";
 //        Function<String, String> objectLine = str -> String.format("object \"%s\" as %s\n", str, str.replaceAll(" ", ""));

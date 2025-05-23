1ce7f136eb379ba7c5b479fc4f96cc43c0154591
mdpiekarz <monika.piekarz@mail.umcs.pl>
2025-05-23 15:35:01 +0200

    lab 11

---
+++ shop/src/main/java/music/Main.java
@@ -0,0 +1,16 @@
+package music;
+
+import auth.AccountManager;
+import database.DatabaseConnection;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        DatabaseConnection connection = new DatabaseConnection();
+        connection.connect("shop.db");
+        AccountManager accountManager = new AccountManager(connection);
+        //accountManager.register("alice", "secret");
+        System.out.println(accountManager.authenticate("alice", "secret"));
+        System.out.println(accountManager.getAccount("alice"));
+    }
+}
